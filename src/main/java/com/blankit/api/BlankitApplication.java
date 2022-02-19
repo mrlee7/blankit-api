c5dda550c14609f265f93236e345f34f541dc12c
mrlee7 <>
2022-02-19 14:46:40 +0900

    no message

--- src/main/java/com/blankit/api/BlankitApplication.java
+++ src/main/java/com/blankit/api/BlankitApplication.java
@@ -2,8 +2,6 @@ package com.blankit.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class BlankitApplication {
