e11ccaaeabb3fb51a9654e2037eb6785f5c0661e
mrlee7 <jplee227@gmail.com>
2022-09-14 12:13:58 +0900

    feat: 스터디그룹 CRUD API

--- src/main/java/com/blankit/api/domain/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blankit.api.domain.repository;
-
-import com.blankit.api.domain.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
--- src/main/java/com/blankit/api/domain/repository/StudyGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blankit.api.domain.repository;
-
-import com.blankit.api.domain.entity.StudyGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long> {
-}
